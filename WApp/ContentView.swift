8f71327a214ac1760aa30db9f31a0e8a12d897ec
mikhail <Morochko2014@gmail.com>
2022-03-05 15:21:19 +0300

    add project files

---
+++ WApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WApp
+//
+//  Created by Mikhail Morochko on 5.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WApp/WAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAppApp.swift
+//  WApp
+//
+//  Created by Mikhail Morochko on 5.03.22.
+//
+
+import SwiftUI
+
+@main
+struct WAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
