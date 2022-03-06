b919324cb356a144ed5e950ca408f08477818a48
mikhail <Morochko2014@gmail.com>
2022-03-06 12:20:46 +0300

    Code refactor

--- WApp/ContentView.swift
+++ WApp/ContentView.swift
@@ -1,5 +1,5 @@
-import SwiftUI
 import MapKit
+import SwiftUI
 
 struct ContentView: View {
     @State private var region = MKCoordinateRegion(
@@ -12,32 +12,40 @@ struct ContentView: View {
             longitudeDelta: 0.5
         )
     )
-    
+
     var body: some View {
         ZStack {
-            Color(red: 28/255, green: 28/255, blue: 35/255, opacity: 1.0)
+            background
                 .ignoresSafeArea()
             VStack {
-                HStack(spacing: 20) {
-                    Text("Grodno")
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 32)
-                        .font(.title)
-                    Text("7°C")
-                        .foregroundColor(.white)
-                        .padding(.trailing, 32)
-                        .font(.title)
-                }
-                .frame(width: 320, height: 90)
-                .background(Color(red: 36/255, green: 34/255, blue: 49/255, opacity: 1.0))
-                .cornerRadius(12)
-                .padding(.bottom, 50)
+                infoBlock
+                    .frame(width: 320, height: 90)
+                    .background(Color(red: 36 / 255, green: 34 / 255, blue: 49 / 255, opacity: 1.0))
+                    .cornerRadius(12)
+                    .padding(.bottom, 50)
                 Map(coordinateRegion: $region)
                     .frame(width: 320, height: 450)
                     .cornerRadius(12)
             }
         }
+    }
+
+    private var infoBlock: some View {
+        HStack(spacing: 20) {
+            Text("London")
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 32)
+                .font(.title)
+            Text("7°C")
+                .foregroundColor(.white)
+                .padding(.trailing, 32)
+                .font(.title)
+        }
+    }
+
+    private var background: some View {
+        Color(red: 28 / 255, green: 28 / 255, blue: 35 / 255, opacity: 1.0)
     }
 }
 
