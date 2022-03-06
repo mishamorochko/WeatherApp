3b2b003f620c697f74525379576b519d294728a7
mikhail <Morochko2014@gmail.com>
2022-03-06 12:05:57 +0300

    Add design ver 1.0, set force dark theme in app

--- WApp/ContentView.swift
+++ WApp/ContentView.swift
@@ -1,16 +1,43 @@
-//
-//  ContentView.swift
-//  WApp
-//
-//  Created by Mikhail Morochko on 5.03.22.
-//
-
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 51.507222,
+            longitude: -0.1275
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5
+        )
+    )
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 28/255, green: 28/255, blue: 35/255, opacity: 1.0)
+                .ignoresSafeArea()
+            VStack {
+                HStack(spacing: 20) {
+                    Text("Grodno")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 32)
+                        .font(.title)
+                    Text("7°C")
+                        .foregroundColor(.white)
+                        .padding(.trailing, 32)
+                        .font(.title)
+                }
+                .frame(width: 320, height: 90)
+                .background(Color(red: 36/255, green: 34/255, blue: 49/255, opacity: 1.0))
+                .cornerRadius(12)
+                .padding(.bottom, 50)
+                Map(coordinateRegion: $region)
+                    .frame(width: 320, height: 450)
+                    .cornerRadius(12)
+            }
+        }
     }
 }
 
